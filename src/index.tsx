9c39f4c4ca7955184653ceb74d3a0189f42d2b8e
hyrious <hyrious@outlook.com>
2022-06-23 11:33:32 +0800

    rename app names

--- src/index.tsx
+++ src/index.tsx
@@ -9,25 +9,25 @@ import styles from "./style.css?inline";
  * Register it before joining room:
  * ```js
  * WindowManager.register({
- *   kind: "HelloWorld",
- *   src: HelloWorld
+ *   kind: "Counter",
+ *   src: Counter
  * })
  * ```
  * Then you can use it in your room:
  * ```js
- * manager.addApp({ kind: 'HelloWorld' })
+ * manager.addApp({ kind: 'Counter' })
  * ```
  * Read more about how to make a netless app here:
  * https://github.com/netless-io/window-manager/blob/master/docs/develop-app.md
  */
-const HelloWorld: NetlessApp = {
-  kind: "HelloWorld",
+const Counter: NetlessApp = {
+  kind: "Counter",
   setup(context) {
     const box = context.getBox();
     box.mountStyles(styles);
 
     const $content = document.createElement("div");
-    $content.className = "app-hello-world";
+    $content.className = "app-counter";
     box.mountContent($content);
 
     const root = createRoot($content);
@@ -40,4 +40,4 @@ const HelloWorld: NetlessApp = {
   },
 };
 
-export default HelloWorld;
+export default Counter;
